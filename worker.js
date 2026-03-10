293c36d2ce83164ccafb2a8895e6e6c464688ccb
Josh Woollett <josh.woollett@beeflambnz.com>
2026-03-10 14:45:54 +1300

    add worker.js for cloudflare

--- worker.js
+++ worker.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+export default {
+  async fetch(request, env, ctx) {
+    return new Response('Hello World')
+  },
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
